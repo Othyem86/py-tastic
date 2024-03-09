93aa2a117b54db93dfa0ecd4a1378c4e429efe2c
Othyem86 <emil.foger@gmail.com>
2024-03-09 11:45:30 +0100

    CHANGE: created LeavNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,13 +4,14 @@ class HTMLNode:
             tag:str = None,
             value:str = None,
             children: list['HTMLNode'] = None,
-            props: dict[str, str] = None) -> None:
+            props: dict[str, str] = None
+        ) -> None:
         self.tag = tag
         self.value = value
         self.childre = children
         self.props = props
 
-    def to_html(self):
+    def to_html(self) -> str:
         raise NotImplementedError()
     
     def props_to_html(self) -> str:
@@ -23,3 +24,23 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(
+            self, 
+            tag: str = None, 
+            value: str = None, 
+            props: dict[str, str] = None
+        )-> None:
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if not self.value:
+            raise ValueError()
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}<{self.tag}/>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
